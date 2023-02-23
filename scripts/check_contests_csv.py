0357889b1829f76635ca732b81acc119a1e1565a
Zhuo Zhang <zhan3299@purdue.edu>
2023-02-23 16:38:22 -0500

    try to add integrity check

---
+++ scripts/check_contests_csv.py
@@ -0,0 +1,38 @@
+import sys
+import os
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <web3bugs.dir>")
+        exit(1)
+
+    web3bugs_dir = sys.argv[1].strip()
+
+    report_dir = os.path.join(web3bugs_dir, "reports")
+    contest_csv = os.path.join(web3bugs_dir, "results/contests.csv")
+
+    all_reports = os.listdir(report_dir)
+    all_contests = set()
+    all_pass = True
+
+    with open(contest_csv) as f:
+        for (lid, line) in enumerate(f):
+            if lid == 0:
+                continue
+
+            contest_id = line.split(",")[0].strip()
+            if f"{contest_id}.md" not in all_reports:
+                print(f"Additional contest in contests.csv: {contest_id}.md")
+                all_pass = False
+
+            all_contests.add(f"{contest_id}.md")
+
+    for report in all_reports:
+        if report not in all_contests:
+            print(f"Missing contest in contests.csv: {report}")
+            all_pass = False
+
+    if all_pass:
+        print(f"Check passed!\n\n\t`{contest_csv}` is consistant with `{report_dir}`")
+    else:
+        exit(1)
---
+++ scripts/check_contracts.py
@@ -0,0 +1,55 @@
+import sys
+import os
+
+NO_CONTRACT = set(["11", "50"])
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} <web3bugs_dir>")
+        exit(1)
+
+    web3bugs_dir = sys.argv[1].strip()
+
+    report_dir = os.path.join(web3bugs_dir, "reports")
+    contract_dir = os.path.join(web3bugs_dir, "contracts")
+
+    all_reports = os.listdir(report_dir)
+    all_contests = os.listdir(contract_dir)
+
+    all_pass = True
+
+    for report in all_reports:
+        if not report.endswith(".md"):
+            print(f"Invalid report name: {report}")
+            exit(1)
+
+        contest_id = report[:-3]
+
+        if contest_id in NO_CONTRACT:
+            continue
+
+        if contest_id not in all_contests:
+            print(f"Missing contract code: {contest_id}")
+            all_pass = False
+
+    for contest_id in all_contests:
+        contest_dir = os.path.join(contract_dir, contest_id)
+
+        find_sol = False
+        for (root, dirs, files) in os.walk(contest_dir):
+            for file_name in files:
+                if file_name.endswith(".sol"):
+                    find_sol = True
+                    break
+
+            if find_sol:
+                break
+
+        if not find_sol:
+            print(f"No solidity file found: {contest_dir}")
+            all_pass = False
+
+    if all_pass:
+        print(f"Check passed!\n\n\t`{contract_dir}` is consistant with `{report_dir}`")
+    else:
+        exit(1)
